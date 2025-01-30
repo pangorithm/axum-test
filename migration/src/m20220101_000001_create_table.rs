fdfac9a80a1a559b9b08e9b79ac073b7c17f8ad2
pangorithm <phs559800@gmail.com>
2025-01-30 20:13:36 +0900

    migration을 위한 설정 파일 작성 및 실행
    https://www.sea-ql.org/SeaORM/docs/migration/running-migration/
    
    cd migration; cargo run -- up
    
    init: Initialize migration directory
    generate: Generate a new migration file
    up: Apply all pending migrations
    up -n 10: Apply 10 pending migrations
    down: Rollback last applied migration
    down -n 10: Rollback last 10 applied migrations
    status: Check the status of all migrations
    fresh: Drop all tables from the database, then reapply all migrations
    refresh: Rollback all applied migrations, then reapply all migrations
    reset: Rollback all applied migrations

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -1,3 +1,25 @@
+use crate::sea_orm::DatabaseBackend;
+use crate::sea_orm::Statement;
+use sea_orm::{EnumIter, Iterable};
+
+#[derive(DeriveIden)]
+enum Post {
+    Table,
+    Id,
+    Title,
+    #[sea_orm(iden = "text")] // Renaming the identifier
+    Text,
+    Category,
+}
+
+#[derive(Iden, EnumIter)]
+pub enum Category {
+    #[iden = "Feed"]
+    Feed,
+    #[iden = "Story"]
+    Story,
+}
+
 use sea_orm_migration::{prelude::*, schema::*};
 
 #[derive(DeriveMigrationName)]
@@ -6,36 +28,66 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
+        // Create the category type
+        manager
+            .get_connection()
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "CREATE TYPE category AS ENUM ('Feed', 'Story')".to_owned(),
+            ))
+            .await?;
 
+        // Create the Post table
         manager
             .create_table(
                 Table::create()
                     .table(Post::Table)
                     .if_not_exists()
-                    .col(pk_auto(Post::Id))
-                    .col(string(Post::Title))
-                    .col(string(Post::Text))
+                    .col(
+                        ColumnDef::new(Post::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Post::Title).string().not_null())
+                    .col(ColumnDef::new(Post::Text).string().not_null())
+                    .col(
+                        ColumnDef::new(Post::Category)
+                            .enumeration(Alias::new("category"), Category::iter()),
+                    )
                     .to_owned(),
             )
-            .await
+            .await?;
+
+        manager
+            .create_index(
+                Index::create()
+                    .if_not_exists()
+                    .name("idx-post_title")
+                    .table(Post::Table)
+                    .col(Post::Title)
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .drop_table(Table::drop().table(Post::Table).to_owned())
-            .await
-    }
-}
+            .await?;
 
-#[derive(DeriveIden)]
-enum Post {
-    Table,
-    Id,
-    Title,
-    Text,
+        // Drop the category type
+        manager
+            .get_connection()
+            .execute(Statement::from_string(
+                DatabaseBackend::Postgres,
+                "DROP TYPE category".to_owned(),
+            ))
+            .await?;
+
+        Ok(())
+    }
 }
